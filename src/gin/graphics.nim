70f9519dd265839810df740fe01f9a5314ffdd37
bob16795 <prestosilver.ptp@gmail.com>
2021-06-14 15:51:58 -0400

    added point class

--- src/gin/graphics.nim
+++ src/gin/graphics.nim
@@ -3,11 +3,18 @@
 # for drawing in gin
 
 type
+  Point* = object
+    X*: int
+    Y*: int
   Rectangle* = object
-    X: int
-    Y: int
-    Width: uint
-    Height: uint
+    X*: int
+    Y*: int
+    Width*: uint
+    Height*: uint
+
+proc InitPoint*(X, Y: int): Point =
+  result.X = X
+  result.Y = Y
 
 proc InitRectangle*(X, Y: int, Width, Height: uint): Rectangle =
   result.X = X
--- tests/testGraphics.nim
+++ tests/testGraphics.nim
@@ -5,7 +5,7 @@ import gin.graphics
 test "point class can create from 2":
     var point = InitPoint(0, 1)
     assert point.X == 0
-    assert point.Y == 0
+    assert point.Y == 1
 
 test "rectangle class can create from 4":
     var rectangle = InitRectangle(0, 0, 4, 4)
