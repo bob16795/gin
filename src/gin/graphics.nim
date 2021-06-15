f066acd73e5c7231d3dbb9bfb29148ef8d4da9b6
bob16795 <prestosilver.ptp@gmail.com>
2021-06-14 20:47:35 -0400

    fixed load texture stuffs

--- src/gin/graphics.nim
+++ src/gin/graphics.nim
@@ -14,6 +14,8 @@ type
     Y*: int
     Width*: uint
     Height*: uint
+  Texture* = object
+    id*: GLuint
 
 proc initGraphics*(): GLFWWindow =
   assert glfwInit()
@@ -26,12 +28,23 @@ proc initGraphics*(): GLFWWindow =
   assert glInit()
   return window
 
-proc loadTexture*(image: cstring): ptr cuchar =
+proc loadTexture*(image: cstring): Texture =
   var width, height: cint
   var c: cint = 0
+  var tex: Texture
+  glGenTextures(1, addr tex.id)
+  glBindTexture(GL_TEXTURE_2D, tex.id)
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT.GLint)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT.GLint)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST.GLint)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST.GLint)
   var img = SOIL_load_image(image, width.addr, height.addr, c.addr, SOIL_LOAD_RGBA)
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA.GLint, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
-  return img
+  if img[].int == 0:
+    return
+  SOIL_free_image_data(img)
+  return tex
 
 proc clearBuffer*(window: GLFWWindow) =
   glClearColor(0.68f, 1f, 0.34f, 1f)
