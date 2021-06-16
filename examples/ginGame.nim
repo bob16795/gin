edd3229075fc21ce59a436ea1010bfe34c41d526
bob16795 <prestosilver.ptp@gmail.com>
2021-06-16 11:46:15 -0400

    cleanup game

--- examples/ginGame.nim
+++ examples/ginGame.nim
@@ -3,21 +3,24 @@ import gin/input
 import gin/graphics
 from sdl2 import Scancode
 
-var kbState: KeyBoardState
-var prevState: KeyBoardState
-
-Setup:
+Game:
     var
-        image: Texture = loadTexture("images/ssss.bmp")
+        image: Texture 
+        bg: Color
+
+        kbState: KeyBoardState
+        prevKbState: KeyBoardState
+
+    template setup(): untyped =
+        image = loadTexture("images/ssss.bmp")
         bg = initColor(255, 255, 255, 255)
 
-Loop:
     template draw(time: cuint, context: GraphicsContext): untyped =
         clearBuffer(bg)
-        renderTexture(image, initRectangle(0, 0, 100, 100), initRectangle(0, 0, 100, 100))
+        renderTexture(image, initRectangle(0, 0, 50, 50), initRectangle(0, 0, 100, 100))
 
     template update(time: cuint): untyped =
-        prevState = kbState
+        prevKbState = kbState
         kbState = getKeyBoardState()
         if kbState.contains(SDL_SCANCODE_ESCAPE):
             endLoop
