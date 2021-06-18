1c8a904fe1d648a9dd245ddc6d35b4bf648ef4b9
bob16795 <prestosilver.ptp@gmail.com>
2021-06-18 15:40:26 -0400

    ?

--- src/gin/templates.nim
+++ src/gin/templates.nim
@@ -8,8 +8,8 @@ template WindowTemplates*(context: GraphicsContext, data: GraphicsInitData): unt
         data.name = d
         
     # set the window size
-    template setWindowSize(size: Point): untyped =
-        data.size = size
+    template setWindowSize(d: Point): untyped =
+        data.size = d
 
     # set the app name IMPORTANT !!!
     template setAppName(name: string): untyped =
