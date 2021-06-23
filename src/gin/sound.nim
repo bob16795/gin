5a82bbf92ab999034219e475f8251edc13485cbe
bob16795 <prestosilver.ptp@gmail.com>
2021-06-23 11:11:08 -0400

    new sound system

--- src/gin/sound.nim
+++ src/gin/sound.nim
@@ -1,53 +1,209 @@
-import sdl2
 import sdl2/audio
-import os
-import storage
+import sdl2
+
+{.experimental: "codeReordering".}
+
+const
+    AUDIO_FORMAT = AUDIO_S16LSB
+    AUDIO_FREQ = 48000
+    AUDIO_CHANNELS = 4
+    AUDIO_SAMPLES = 4096
+    AUDIO_MAX_SOUNDS = 25
+    AUDIO_MUSIC_FADE_VALUE = 2
+    SDL_AUDIO_ALLOW_CHANGES = SDL_AUDIO_ALLOW_ANY_CHANGE
 
 type
-    SoundEffect* = object
-        spec: AudioSpec
-        length: uint32
-        buffer: ptr uint8
-    AudioData = object
-        length: uint32
-        position: ptr uint8
+    PrivateAudioDevice* = object
+        device*: AudioDeviceID
+        want*: AudioSpec
+        audioEnable*: uint8
+    Audio* = object
+        length*: uint32
+        lengthTrue*: uint32
+        bufferTrue*: ptr uint8
+        buffer*: ptr uint8
+        loop*: uint8
+        fade*: uint8
+        free*: uint8
+        volume*: uint8
+        audio*: AudioSpec
+        next*: ptr Audio
 
-var 
-    audioDevice: AudioDeviceID
+var
+    gDevice: ptr PrivateAudioDevice
+    gSoundCount: uint32
 
-proc initSound*() =
-    init(INIT_AUDIO)
+proc playSound*(filename: cstring, volume: cint) =
+    playAudio(filename, nil, 0, volume)
 
-proc callback(userdata: pointer, stream: ptr uint8, len: cint) {.cdecl.} =
-    var data = cast[AudioData](userdata)
-    if data.length == 0:
+proc playMusic*(filename: cstring, volume: cint) =
+    playAudio(filename, nil, 1, volume)
+
+proc playSoundFromMemory*(audio: ptr Audio, volume: cint) =
+    playAudio(nil, audio, 0, volume)
+
+proc playMusicFromMemory*(audio: ptr Audio, volume: cint) =
+    playAudio(nil, audio, 1, volume)
+
+proc initAudio*() =
+    var global: ptr Audio
+    gDevice = cast[ptr PrivateAudioDevice](alloc(sizeof(PrivateAudioDevice)))
+    gSoundCount = 0
+    if gDevice == nil:
+        echo "AUDIO COULDNT BE LOADED"
         return
-    var length = len.uint32
-    if length > data.length:
-        length = data.length
+    gDevice.want.freq = AUDIO_FREQ
+    gDevice.want.format = AUDIO_FORMAT
+    gDevice.want.channels = AUDIO_CHANNELS
+    gDevice.want.samples = AUDIO_SAMPLES
+    gDevice.want.callback = audioCallback
+    gDevice.want.userdata = alloc(sizeof(Audio))
+    global = cast[ptr Audio](gDevice.want.userdata)
+    if global == nil:
+        return
+    global.buffer = nil
+    global.next = nil
+    gDevice.device = openAudioDevice(nil, 0, addr(gDevice.want), nil, SDL_AUDIO_ALLOW_CHANGES)
+    if gDevice.device == 0:
+        return
+    else:
+        gDevice.audioEnable = 1
+        unpauseAudio()
 
-    mixAudio(stream, data.position, length, SDL_MIX_MAXVOLUME)
+proc endAudio*() =
+    if gDevice.audioEnable != 0:
+        pauseAudio()
+        freeAudio(cast[ptr Audio](gDevice.want.userdata))
+        closeAudioDevice(gDevice.device)
 
-    data.position[] += length.uint8
-    data.length -= length
+proc pauseAudio*() =
+    if gDevice.audioEnable != 0:
+        pauseAudioDevice(gDevice.device, 1)
 
-proc initSoundEffect*(path: string): SoundEffect =
-    var
-        spec: AudioSpec
-        length: uint32
-        buffer: ptr uint8
-    discard loadWAV(getFullFilePath(path), addr spec, addr buffer, addr length)
-    result.spec = spec
-    spec.callback = callback
-    result.length = length
-    result.buffer = buffer
-    if audioDevice == 0:
-        audioDevice = openAudioDevice(nil, 0, addr result.spec, nil, 0)
+proc unpauseAudio*() =
+    if gDevice.audioEnable != 0:
+        pauseAudioDevice(gDevice.device, 0)
 
+proc freeAudio*(a: ptr Audio) =
+    var temp, audio: ptr Audio
+    audio = a
+    while audio != nil:
+        if audio.free == 1:
+            freeWAV(audio.bufferTrue)
+        temp = audio
+        audio = audio.next
+        discard temp.free
 
-proc play*(effect: SoundEffect) =
-    var success = queueAudio(audioDevice, effect.buffer, effect.length)
-    pauseAudioDevice(audioDevice, 0)
+proc createAudio*(filename: cstring, loop: uint8, volume: cint): ptr Audio =
+    var newAudio: ptr Audio = cast[ptr Audio](alloc(sizeof(Audio)))
+    if newAudio == nil:
+        echo "couldnt load audio: " & $filename
+        return nil
+    if filename == nil:
+        echo "filename is nil"
+        return nil
+    newAudio.next = nil
+    newAudio.loop = loop
+    newAudio.fade = 0
+    newAudio.free = 1
+    newAudio.volume = volume.uint8
+    if loadWav($filename, addr newAudio.audio, addr newAudio.bufferTrue, addr newAudio.lengthTrue) == nil:
+        echo "wav couldnt be loaded"
+        discard newAudio.free
+        return nil
+    
+    newAudio.buffer = newAudio.bufferTrue
+    newAudio.length = newAudio.lengthTrue
+    newAudio.audio.callback = nil
+    newAudio.audio.userdata = nil
+    return newAudio
 
-proc pause*(effect: SoundEffect) =
-    pauseAudioDevice(audioDevice, 1)
+proc playAudio(filename: cstring, audio: ptr Audio, loop: uint8, volume: cint) =
+    var newAudio: ptr Audio
+    if gDevice.audioEnable == 0:
+        return
+    if loop == 0:
+        if gSoundCount >= AUDIO_MAX_SOUNDS:
+            return
+        else:
+            inc gSoundCount
+    if filename != nil:
+        newAudio = createAudio(filename, loop, volume)
+    elif audio != nil:
+        newAudio[] = audio[]
+        newAudio.volume = volume.uint8
+        newAudio.loop = loop
+        newAudio.free = 0
+    else:
+        echo "all audio params are null"
+        return
+    lockAudioDevice(gDevice.device)
+    if loop == 1:
+        addMusic(cast[ptr Audio](gDevice.want.userdata), newAudio)
+    else:
+        addAudio(cast[ptr Audio](gDevice.want.userdata), newAudio)
+    unlockAudioDevice(gDevice.device)
+
+proc addMusic(root: ptr Audio, newAudio: ptr Audio) =
+    var musicFound: uint8 = 0
+    var rootNext: ptr Audio = root.next
+
+    while rootNext != nil:
+        if rootNext.loop == 1 and rootNext.fade == 0:
+            if musicFound != 0:
+                rootNext.length = 0
+                rootNext.volume = 0
+            rootNext.fade = 1
+        elif rootNext.loop == 1 and rootNext.fade == 1:
+            musicFound = 1
+        rootNext = rootNext.next
+    addAudio(root, newAudio)
+
+proc audioCallback(userdata: pointer, stream: ptr uint8, len: cint) {.cdecl.} =
+    var audio: ptr Audio = cast[ptr Audio](userdata)
+    var previous: ptr Audio = audio
+    var tempLength: uint32
+    var music: uint8 = 0
+    zeroMem(stream, len)
+    audio = audio.next
+    while audio != nil:
+        if audio.length > 0:
+            if audio.fade == 1 and audio.loop == 1:
+                music = 1
+                if audio.volume > 0:
+                    if audio.volume - AUDIO_MUSIC_FADE_VALUE < 0:
+                        audio.volume = 0
+                    else:
+                        dec(audio.volume, AUDIO_MUSIC_FADE_VALUE)
+                else:
+                    audio.length = 0
+            if music != 0 and audio.loop == 1 and audio.fade == 0:
+                tempLength = 0
+            else:
+                if (cast[uint32](len) > audio.length):
+                    tempLength = audio.length
+                else:
+                    tempLength = cast[uint32](len)
+            mixAudioFormat(stream, audio.buffer, AUDIO_FORMAT, tempLength, audio.volume.cint)
+            audio.buffer[] += tempLength.uint8
+            audio.length -= tempLength
+            previous = audio
+            audio = audio.next
+        elif audio.loop == 1 and audio.fade == 0:
+            audio.buffer = audio.bufferTrue
+            audio.length = audio.lengthTrue
+        else:
+            previous.next = audio.next
+            if (audio.loop == 0):
+                dec(gSoundCount)
+            audio.next = nil
+            freeAudio(audio)
+            audio = previous.next
+
+proc addAudio*(r: ptr Audio, newAudio: ptr Audio) =
+    var root = r
+    if root == nil:
+        return
+    while root.next != nil:
+        root = root.next
+    root.next = newAudio
